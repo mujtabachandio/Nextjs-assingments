6faddea6c469d99b4e86ca3fdf33ebfd241882b6
Your Name <your.email@example.com>
2024-09-20 03:29:11 +0500

    update

---
+++ assingment01/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen text-center bg-gray-100 overflow-hidden">
+    <h1 className="text-5xl animate-fade-in">Hello world</h1>
+    <h2 className="text-2xl animate-fade-in delay-100">Assignment number one Done By: <span className="flex flex-col items-center justify-center text-center animate-pulse text-red-800">Mujtaba Chandio</span></h2>
+    <h1 className="text-4xl animate-bounce">🐼</h1>
+  </div>
+  );
+}
--- assingment2/app/page.tsx
+++ assingment2/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen text-center bg-gray-100 overflow-hidden">
       <h1 className="text-5xl animate-fade-in">Assingmet 2 Components</h1>
-      <h2 className="text-2xl animate-fade-in delay-100">Assignment number one Done By: <span className="flex flex-col items-center justify-center text-center animate-pulse text-red-800">Mujtaba Chandio</span></h2>
+      <h2 className="text-2xl animate-fade-in delay-100">Assignment number Two Done By: <span className="flex flex-col items-center justify-center text-center animate-pulse text-red-800">Mujtaba Chandio</span></h2>
       <h1 className="text-4xl animate-bounce">🐼</h1>
     </div>
   );
